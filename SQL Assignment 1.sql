dc4c50dc8228104315726aaec630a56584c44b99
Framos22 <you@example.com>
2022-04-08 23:20:49 -0700

    MySQL Assignment

---
+++ SQL Assignment 1.sql
@@ -0,0 +1,11 @@
+show databases;
+show tables;
+SELECT * FROM employees;
+SELECT * FROM employees WHERE birth_date <= '1965-01-01';
+SELECT * FROM employees WHERE hire_date > '1989-12-31';
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'F%' LIMIT 50;
+INSERT INTO employees VALUES(100, '1992-09-15', 'Felix', 'Ramos', 'M', '2022-07-08'), (101, '1992-02-04', 'Victoria', 'Ramos', 'F', '2015-08-05'), (102, '2022-01-03', 'Oli', 'Ramos', 'F', '2022-04-08');
+UPDATE employees SET first_name = 'Bob' WHERE emp_no = 10023;
+UPDATE employees SET hire_date = '2002-01-01' WHERE first_name LIKE 'P%' OR last_name LIKE 'P%';
+DELETE FROM employees WHERE emp_no < 10000;
+DELETE FROM employees WHERE emp_no IN (10048, 10099, 10234, 20089);
